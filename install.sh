37427fed1e9a4afbfe45e83c1ac99b367e659d51
wubaiqing <wubaiqing@vip.qq.com>
2013-06-17 10:20:31 +0800

    edit install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
-function install() {
-    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+function install()
+{
+    read -p "This may overwrite existing files in your home directory! (y/n) " -n 1
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         rsync --exclude ".DS_Store" --exclude ".git/" --exclude ".gitignore" --exclude ".gitmodules" --exclude "install.sh" --exclude "README.md" -av . ~
         echo 'installed'
