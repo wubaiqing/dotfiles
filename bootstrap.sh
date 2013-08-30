284d1874800e4f6dde5e1a2a6d99dcdf823a28ff
wubaiqing <wubaiqing@vip.qq.com>
2013-08-30 11:25:42 +0800

    add brew install tree command

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,7 +5,7 @@ function doIt()
 {
     read -p "This may overwrite existing files in your home directory! (y/n) " -n 1
     if [[ $REPLY =~ ^[Yy]$ ]]; then
-        rsync --exclude ".DS_Store" --exclude ".git/" --exclude ".gitignore" --exclude ".gitmodules" --exclude "install.sh" --exclude "README.md" -av . ~
+        rsync --exclude ".DS_Store" --exclude ".git/" --exclude ".gitignore" --exclude ".gitmodules" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
         echo 'installed'
     fi
 }
